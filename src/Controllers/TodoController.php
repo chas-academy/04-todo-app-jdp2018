f8342eb21bd9a24806eee03c55650bbbdf1073c7
Diego <jd@Diegos-MacBook-Pro.local>
2019-01-21 14:02:59 +0100

    Done

--- src/Controllers/TodoController.php
+++ src/Controllers/TodoController.php
@@ -4,8 +4,8 @@ use Todo\Controller;
 use Todo\Database;
 use Todo\TodoItem;
 
-class TodoController extends Controller {
-    
+class TodoController extends Controller
+{
     public function get()
     {
         $todos = TodoItem::findAll();
@@ -18,44 +18,31 @@ class TodoController extends Controller {
         $result = TodoItem::createTodo($body['title']);
 
         if ($result) {
-          $this->redirect('/');
+            TodoItem::createTodo($todoId);
         }
+        $this->redirect('/');
     }
 
     public function update($urlParams)
     {
         $body = filter_body(); // gives you the body of the request (the "envelope" contents)
         $todoId = $urlParams['id']; // the id of the todo we're trying to update
-        $completed = isset($body['status']) ? 1 : 0; // whether or not the todo has been checked or not
+        $completed = isset($body['status']) ? 'true' : 'false'; // whether or not the todo has been checked or not
+        $title =$body['title'];
 
-        // TODO: Implement me!
-        // This action should update a specific todo item in the todos table using the TodoItem::updateTodo method.
-        // Try and figure out what parameters you need to pass to the updateTodo-method in the TodoItem model.
-
-        // if there's a result
-          // use the redirect method to send the user back to the list of todos $this->redirect('/');
-        // otherwise, throw an exception or show an error message
+        
+        TodoItem::updateTodo($todoId, $title, $completed);
+        $this->redirect('/');   
+        
     }
 
     public function delete($urlParams)
     {
-      // TODO: Implement me!
-    }
+        $body = filter_body();
+        $todoId = $urlParams['id'];
+        $completed = isset($body['status']) ? 1 : 0;
+        TodoItem::deleteTodo($todoId);
 
-    /**
-     * OPTIONAL Bonus round!
-     * 
-     * The two methods below are optional, feel free to try and complete them
-     * if you're aiming for a higher grade.
-     */
-    public function toggle()
-    {
-      // (OPTIONAL) TODO: This action should toggle all todos to completed, or not completed.
+        $this->redirect('/');
     }
-
-    public function clear()
-    {
-      // (OPTIONAL) TODO: This action should remove all completed todos from the table.
-    }
-
 }
